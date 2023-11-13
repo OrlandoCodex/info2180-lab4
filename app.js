6b793a5d3be9b60dac1177b9fbadcbfaaf401554
Orlando Drummonds <orlandodrummonds2015@gmail.com>
2023-11-13 07:53:13 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-function SearchDoc(){
+/*function SearchDoc(){
         const xhttp = new XMLHttpRequest();
         xhttp.onload = function() {
             alert(this.responseText);
         }
         xhttp.open("GET","superheroes.php");
         xhttp.send();
-    }
+    }*/
+
+var input = document.getElementById("textfield").value;
+const searchbtn = document.getElementById("searchbtn");
+searchbtn.addEventListener("click",btnfunction);
+function btnfunction(e){
+    //e.preventDefault();
+    //const xhttp = new XMLHttpRequest();
+    if(input.trim() == ""){
+        //xhttp.onload = function() {
+            document.getElementById("result").innerHTML = this.responseText;
+          
+          //xhttp.open("GET", "superheroes.php");
+         // xhttp.send();
+    }else{
+        /*xhttp.onload = function() {
+            document.getElementById("result").innerHTML = this.responseText;
+          }
+          xhttp.open("POST", "superheroes.php");
+          xhttp.send(input);
+        }*/
         
+    }
+}
 
 
